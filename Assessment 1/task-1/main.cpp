728a57d50c1997f9626ad53f59a56beb3c610436
Steezyy17 <steveb1701@outlook.com>
2023-10-06 22:52:16 +0100

    First Push
    Assessment 1:
    
    Challenges 1-5 (Completed)
    Challenge 6 start

---
+++ Assessment 1/task-1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void main()
+{
+    //Add code here to print out an ASCII art game header
+    //of your choice. Characters should be aligned correctly
+    //such that when the application is run, it prints out a legible
+    //ascii art game header.
+    //---
+    // Some useful resources:
+    //- https://patorjk.com/software/taag/
+    //- https://www.asciiart.eu/
+
+
+    
+}
